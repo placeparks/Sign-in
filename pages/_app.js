7b4772e5d8a3d6be50e186430322917eaaa51984
placeparks <86215469+placeparks@users.noreply.github.com>
2023-02-08 02:59:08 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
       desiredChainId={ChainId.Mumbai}
       authConfig={{
         authUrl: "/api/auth",
-        domain: "localhost:3001",
+        domain: "https://sign-in-b4xz-25sr119wo-placeparks.vercel.app/",
         loginRedirect: "/main",
       }}
     >
