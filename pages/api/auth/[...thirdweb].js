e6664bceb4cb6987bb683486562dfe83b062d6df
placeparks <86215469+placeparks@users.noreply.github.com>
2023-02-08 20:14:15 +0500

    Update [...thirdweb].js

--- pages/api/auth/[...thirdweb].js
+++ pages/api/auth/[...thirdweb].js
@@ -2,7 +2,7 @@ import { ThirdwebAuth } from "@thirdweb-dev/auth/next";
 
 export const { ThirdwebAuthHandler } = ThirdwebAuth({
   privateKey: process.env.NEXT_ADMIN_PRIVATE_KEY,
-  domain: "https://vercel.com/placeparks/sign-in-b4xz",
+  domain: "https://sign-in-b4xz-25sr119wo-placeparks.vercel.app/",
 });
 
 export default ThirdwebAuthHandler();
