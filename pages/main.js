3cb5fe751d67ffe9eef3b8b2dcd5a0af5e024100
placeparks <86215469+placeparks@users.noreply.github.com>
2023-02-08 19:54:07 +0500

    Update main.js

--- pages/main.js
+++ pages/main.js
@@ -9,7 +9,7 @@ return(
     <h3>Choose Your Desired Page</h3>
 <a target="_blank" href="https://ipfs-dapp-wine.vercel.app/mint" rel="noopener noreferrer"><button class="btn-mint">Go to Mint page</button></a> 
 
-<a target="_blank" href="https://gallery-gamma-bay.vercel.app/"><button class="btn-gallery" rel="noopener noreferrer" >Nft Gallery</button></a> 
+<a target="_blank" href="https://gallery-gamma-bay.vercel.app/" rel="noreferrer"><button class="btn-gallery" rel="noopener noreferrer" >Nft Gallery</button></a> 
 
 <a target="_blank" href="https://ipfs-dapp-wine.vercel.app/" rel="noopener noreferrer"><button class="btn-ipfs" >Ipfs Drop</button></a>
 
