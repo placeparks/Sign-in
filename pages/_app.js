932d88eb481233b25ced5c63b1f3c9a267ef5849
placeparks <placeparks@gmail.com>
2023-08-31 00:53:44 +0500

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,11 @@ import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const Key= process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
   return (
     <ThirdwebProvider
       desiredChainId={ChainId.Mumbai}
+      clientId= {Key}
       authConfig={{
         authUrl: "/api/auth",
         domain: "https://sign-in-b4xz-25sr119wo-placeparks.vercel.app/",
--- pages/main.js
+++ pages/main.js
@@ -1,17 +1,12 @@
-import Link from 'next/link'
 import Image from "next/image"
 
 export default function main(){
 return(
 <div>
 <div class="page">
-    <h1>Wecome To Mirac.eth App</h1>
-    <h3>Choose Your Desired Page</h3>
-<a target="_blank" href="https://ipfs-dapp-wine.vercel.app/mint" rel="noopener noreferrer"><button class="btn-mint">Go to Mint page</button></a> 
+    <h1>Welcome, Adventurer! 🚀</h1>
 
-<a target="_blank" href="https://gallery-gamma-bay.vercel.app/" rel="noreferrer"><button class="btn-gallery" rel="noopener noreferrer" >Nft Gallery</button></a> 
-
-<a target="_blank" href="https://ipfs-dapp-wine.vercel.app/" rel="noopener noreferrer"><button class="btn-ipfs" >Ipfs Drop</button></a>
+<h3>You've just unlocked a universe of possibilities. Let's explore it together! 😄</h3>
 
 </div>
 <div class="footer"><a target="_blank" href="https://bio.link/099" rel="noreferrer"><Image src="/mail.png" height={22} width={20} alt="contact"/></a>
